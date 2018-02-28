eb8717d5ca6928e4cd1560b911110e4a1766493e
Rodrigo Samayoa <jorosafi@gmail.com>
2018-02-28 11:20:51 -0800

    add content to page-submit.php

--- page-submit.php
+++ page-submit.php
@@ -10,11 +10,44 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+			<section class="quote-submission-wrapper">
+        <header>
+          <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+        </header>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+        <?php if(is_user_logged_in() && current_user_can('edit_posts')):?>
 
-			<?php endwhile; // End of the loop. ?>
+          <form name="quoteForm" id="quote-submission-form">
+            <div>
+              <label for="quote-author">Author of Quote</label>
+              <input type="text" name="quote_author" id="quote-author">
+            </div>
+            <div>
+              <label for="quote-content">Quote</label>
+              <textarea name="quote_content" id="quote-content" cols="20" rows="3"></textarea>
+            </div>
+            <div>
+              <label for="quote-source">Quote Source</label>
+              <input type="text" name="quote_source" id="quote-source">
+            </div>
+            <div>
+              <label for="quote-source-url">Quote Source URL (if available)</label>
+              <input type="url" name="quote_source_url" id="quote-source-url">
+            </div>
+            <input type="submit" value="Submit Quote">
+          </form>
+
+          <p class="submit-success-message" style="display:none;"></p>
+
+        <?php else: ?> 
+          <p>Sorry, you must be logged in to submit a quote :(</p>
+          
+          <p> <?php echo sprintf('<a href="%1s">%2s</a>', esc_url(wp_login_url), 'Click here to login') ?> </p>
+
+        <?php endif; ?>
+
+
+      </section>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
